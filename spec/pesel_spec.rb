b42701b31530542c302d7bb671ed8086e19b7f4d
Mario <marioosh@5dots.pl>
2011-03-30 21:52:31 +0200

    added pesel spec

---
+++ spec/pesel_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+require 'rspec'
+
+describe "Pesel"  do
+
+  it "should be valid"do
+    valid_numbers = %w[44051401359]
+    valid_numbers.each do |n|
+        pesel = MagickNumbers::Pesel.new(n)
+        pesel.should be_valid
+    end
+  end
+
+  it "should be invalid" do
+    invalid_numbers = %w[44051401358]
+    invalid_numbers.each do |n|
+      pesel = MagickNumbers::Pesel.new(n)
+      pesel.should_not be_valid
+    end
+  end
+end
