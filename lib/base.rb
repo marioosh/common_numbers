fad1f2e0cc7d5dd771f36caa7d269f342fc1ffe8
Mario <marioosh@5dots.pl>
2011-03-30 22:28:32 +0200

    refectored members up

--- lib/base.rb
+++ lib/base.rb
@@ -1,7 +1,29 @@
 module MagickNumbers
   
   class Base
+    
+    attr_accessor :magick_number, :regexp, :mask, :modulo
 
+    def valid?
+      validate
+    end
+
+    def validate
+      validate_regexp && validate_length && validate_sum_control
+    end
+
+    def validate_regexp
+        regexp =~ magick_number
+    end
+
+    def validate_sum_control
+      magick_number.slice!(0..1) if international?
+      nip = magick_number.split( "").collect &:to_i
+      checksum = mask.inject(0) {|sum, weight| sum + weight * nip.shift}
+      mod = checksum % modulo
+      mod = 0 if mod == 10
+      mod === nip.shift
+    end
   end
 
 end
--- lib/nip.rb
+++ lib/nip.rb
@@ -1,40 +1,25 @@
 module MagickNumbers
-  class Nip
-    MASK = [ 6, 5, 7, 2, 3, 4, 5, 6, 7]
-    MODULO = 11 
-    REGEXP = /^(PL)?[0-9]*/
+  class Nip < MagickNumbers::Base
 
-    def initialize(number)
-      @number = number.to_s.gsub('-', '')
+    def initialize(num)
+
+      @mask = [ 6, 5, 7, 2, 3, 4, 5, 6, 7]
+      @modulo = 11 
+      @regexp = /^(PL)?[0-9]*/
+      @magick_number = num.to_s.gsub('-', '')
+
     end
 
-    def valid?
-      validate
-    end
 
-private 
-  def validate
-    validate_regexp && validate_length && validate_sum_control
-  end
-
-  def validate_regexp
-      REGEXP =~ @number
-  end
 
   def validate_length
-    @number.size == (international? ? 12 : 10)
+    @magick_number.size == (international? ? 12 : 10)
   end
   
   def international?
-    /^PL/ =~ @number
+    /^PL/ =~ @magick_number
   end
 
-  def validate_sum_control
-    @number.slice!(0..1) if international?
-    nip = @number.split( "").collect &:to_i
-    checksum = MASK.inject(0) {|sum, weight| sum + weight * nip.shift}
-    checksum % MODULO === nip.shift
-  end
 
 
 end
