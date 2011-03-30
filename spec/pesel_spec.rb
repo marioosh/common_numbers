d15c4e37270e4025e68ec0f0b86ad34ff2264cfb
Mario <marioosh@5dots.pl>
2011-03-31 00:26:50 +0200

    added another pesel number to test

--- spec/pesel_spec.rb
+++ spec/pesel_spec.rb
@@ -4,7 +4,7 @@ require 'rspec'
 describe "Pesel"  do
 
   it "should be valid"do
-    valid_numbers = %w[75090107377]
+    valid_numbers = %w[44051401359 02070803628]
     valid_numbers.each do |n|
         pesel = MagickNumbers::Pesel.new(n)
         pesel.should be_valid
@@ -12,7 +12,7 @@ describe "Pesel"  do
   end
 
   it "should be invalid" do
-    invalid_numbers = %w[44051401358]
+    invalid_numbers = %w[44051401358 02070803629]
     invalid_numbers.each do |n|
       pesel = MagickNumbers::Pesel.new(n)
       pesel.should_not be_valid
